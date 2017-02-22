28c59ee0a9234b792acb412b09e8f7775c990b7d
Andrei Hajdukewycz <sancus@off.net>
2017-02-22 00:16:50 +0000

    add update script and autoconfig.thunderbird.net

---
+++ files/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DEST=/var/www/html/autoconfig.momo
+DATA=/var/www/autoconfig.momo
+cd $DATA
+rm -rf convert.py
+wget https://svn.mozilla.org/mozillamessaging.com/sites/ispdb.mozillamessaging.com/trunk/tools/convert.py
+cd $DATA/trunk
+svn up
+
+mkdir -p $DEST/v1.1
+mkdir -p $DEST/v1.0
+
+source /var/www/tbservices/bin/activate
+python ../convert.py -a -d $DEST/v1.1 *
+rm -rf $DATA/trunk/cloudnine-net.jp
+python ../convert.py -a -d $DEST/v1.0 -v 1.0 *
