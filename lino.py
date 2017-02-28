033e01504fbac2c17a7c502e085d3ca99cfc1575
Andrei Hajdukewycz <sancus@off.net>
2017-02-28 21:50:27 +0000

    add cloudflare dns deletion to lino.py

--- lino.py
+++ lino.py
@@ -1,13 +1,20 @@
 
-import linode.api
-import yaml
 import argparse
+import linode.api
+import json
+import requests
 import sys
+import yaml
+
 from distutils.util import strtobool
 
 # For the API key
 secrets_file = 'vars/secrets.yml'
 vars_file = 'vars/conf.yml'
+
+#cloudflare settings
+cloudflare_zone_identifier = '10eb4a11ab885340bd57de0c72a4ee55'
+cloudflare_url = 'https://api.cloudflare.com/client/v4'
 
 # SSL key/certs for the load balancer
 key_file = '../dehydrated/certs/sancus.ca/privkey.pem'
@@ -23,13 +30,20 @@ with open(fullchain_file, 'r') as f:
 with open(secrets_file, 'r') as f:
 	doc = yaml.load(f)
 	api_key = doc['linode_api_key']
+	cloudflare_api_key = doc['cloudflare_api_key']
+	cloudflare_email = doc['cloudflare_email']
 with open(vars_file, 'r') as f:
 	doc = yaml.load(f)
+	server_domain = doc['server_domain']
 	datacenter = doc['linode_datacenter']
 
 api = linode.api.Api(api_key)
+cloudflare_headers = {'X-Auth-Email': cloudflare_email,
+					  'X-Auth-Key': cloudflare_api_key,
+					  'Content-Type': 'application/json'}
 
 def nodebalancer_config(balancerid, **kwargs):
+	# https://www.linode.com/api/nodebalancer/nodebalancer.config.create
 	port = kwargs.get('port', 80)
 	protocol = kwargs.get('protocol', 'http')
 	algorithm = kwargs.get('algorithm', 'roundrobin')
@@ -77,6 +91,33 @@ def main():
 		reboot(args.action[1:])
 	else:
 		sys.exit('No known action requested. Maybe you meant delete, listnodes, or nodecreate?')
+
+def cf_get_identifier(hostname):
+	# Get the identifier for an A record, takes the first one listed by cf
+	identifier_url = "{0}/zones/{1}/dns_records".format(cloudflare_url, cloudflare_zone_identifier)
+	response = requests.get(identifier_url, headers=cloudflare_headers)
+	fullname = hostname + '.{0}'.format(server_domain)
+	if response:
+		data = json.loads(response.text)
+		for record in data['result']:
+			if record['name'] == fullname:
+				return record['id']
+		return False
+	else:
+		return False
+
+def cf_delete_dns_record(record_id):
+	# Delete cloudflare A record by identifier from cf_get_identifier
+	delete_url = "{0}/zones/{1}/dns_records/{2}".format(cloudflare_url, cloudflare_zone_identifier, record_id)
+	response = requests.delete(delete_url, headers=cloudflare_headers)
+	if response:
+		data = json.loads(response.text)
+		if data['success']:
+			return True
+		else:
+			return False
+	else:
+		return False
 
 def reboot(labels):
 	if not labels:
@@ -169,20 +210,23 @@ def listnodes():
 
 def delete(labels):
 	vpsids = {}
-	todelete = {}
+	todelete = {} # dict label:linodeid of the nodes to be deleted
 
 	for vps in api.linode_list():
 		vpsids[vps['LABEL']] = vps['LINODEID']
 	for label in labels:
 		for key, value in vpsids.iteritems():
 			if label in key:
-				todelete[key] = value
+				todelete[label] = value
 	if todelete:
 		print todelete
 		if prompt('Are you sure you want to delete the above instances?'):
 			for key, value in todelete.iteritems():
 				r = api.linode_delete(LINODEID=value, skipChecks=1)
-				print r
+				if r:
+					print r
+					if cf_delete_dns_record(cf_get_identifier(key)):
+						print "{0} DNS record deleted.".format(key)
 	else:
 		print "Can't delete, no linodes matched: " + " ".join(labels[1:])
 
