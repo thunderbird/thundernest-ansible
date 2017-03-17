36124fd78c617f804510500351a269836e03ff27
Andrei Hajdukewycz <sancus@off.net>
2017-03-17 21:20:05 +0000

    fix directory requirements in cert helper script

--- helpers/certs.py
+++ helpers/certs.py
@@ -3,11 +3,13 @@ import os
 
 def get():
     python3env = os.environ.copy()
-    python3env['PATH'] = '/home/ansibler/letsencrypt/bin:' + python3env['PATH']
+    python3dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/letsencrypt/bin:'
+
+    python3env['PATH'] = python3dir + python3env['PATH']
 
     commands = [
             ["~/dehydrated/dehydrated --register --accept-terms"],
-            ["~/dehydrated/dehydrated -c -t dns-01 -k '/home/ansibler/dehydrated/hooks/cloudflare/hook.py'"],
+            ["~/dehydrated/dehydrated -c -t dns-01 -k '../dehydrated/hooks/cloudflare/hook.py'"],
     ]
 
     executor.run(commands, env=python3env, shell=True)
