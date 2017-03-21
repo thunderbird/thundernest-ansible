9aad1efc7b7cd898434b37eee618e5aaf4352e58
Andrei Hajdukewycz <sancus@off.net>
2017-03-21 08:06:38 +0000

    add stage and test configurations and change name of build script

---
+++ build_servers.py
@@ -0,0 +1,47 @@
+import helpers.executor
+import helpers.certs
+import os
+import sys
+
+try:
+	action = sys.argv[1]
+except IndexError:
+	sys.exit('You must enter which cluster to build: production, test, or stage')
+
+if action == 'test':
+	webhead_plan = 1
+	webhead_names = ['webtest1', 'webtest2']
+	runcommands = [
+	        ['ansible-playbook','--extra-vars', 'server_hostname={0} linode_plan={1}'.format(webhead_names[0], webhead_plan), 'provision-server.yml'],
+	        ['ansible-playbook','--extra-vars', 'server_hostname={0} linode_plan={1}'.format(webhead_names[1], webhead_plan), 'provision-server.yml'],
+	        ['ansible-playbook','-i', '{0}.thunderbird.net,{1}.thunderbird.net'.format(webhead_names[0], webhead_names[1]),
+	         '--extra-vars', 'var_hosts=all', 'setup-webheads.yml'],
+		]
+	
+if action == 'production':
+	if len(sys.argv) < 4:
+		sys.exit('You must enter two names for the webheads in addition to the production action')
+	webhead_names = []
+	webhead_names.append(sys.argv[2])
+	webhead_names.append(sys.argv[3])
+	webhead_plan = 4
+	runcommands = [
+	        ['ansible-playbook','--extra-vars', 'server_hostname={0} linode_plan={1}'.format(webhead_names[0], webhead_plan), 'provision-server.yml'],
+	        ['ansible-playbook','--extra-vars', 'server_hostname={0} linode_plan={1}'.format(webhead_names[1], webhead_plan), 'provision-server.yml'],
+	        ['ansible-playbook','-i', '{0}.thunderbird.net,{1}.thunderbird.net'.format(webhead_names[0], webhead_names[1]),
+	         '--extra-vars', 'var_hosts=all', 'setup-webheads.yml'],
+		]
+
+if action == 'stage':
+	webhead_plan = 1
+	webhead_names = ['webstage1, webstage2']
+	runcommands = [
+	        ['ansible-playbook','--extra-vars', 'server_hostname={0} linode_plan={1}'.format(webhead_names[0], webhead_plan), 'provision-server.yml'],
+	        ['ansible-playbook','--extra-vars', 'server_hostname={0} linode_plan={1}'.format(webhead_names[1], webhead_plan), 'provision-server.yml'],
+	        ['ansible-playbook','-i', '{0}.thunderbird.net,{1}.thunderbird.net'.format(webhead_names[0], webhead_names[1]),
+	         '--extra-vars', 'var_hosts=all', 'setup-webheads.yml'],
+		]
+
+
+
+helpers.executor.run(runcommands, env=os.environ.copy())
