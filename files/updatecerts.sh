c415624c2986df62a71fbd66044308d96513e43a
Andrei Hajdukewycz <sancus@off.net>
2017-11-21 18:34:42 -0800

    Add script for automated SSL renewal.

--- files/updatecerts.sh
+++ files/updatecerts.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 source ~/letsencrypt/bin/activate
 source ~/thundernest-ansible/files/secrets.sh
-cd ~/letsencrypt && ./dehydrated --force -c -t dns-01 -k 'hooks/cloudflare/hook.py'
-
+cd ~/dehydrated && ./dehydrated --force -c -t dns-01 -k 'hooks/cloudflare/hook.py'
+cd ~/thundernest-ansible
+cp ~/dehydrated/certs/thunderbird.net/{cert,chain,fullchain,privkey}.pem files
+ansible-playbook --extra-vars="var_hosts=stage" plays/refresh-ssl-certs.yml
