97a32d69bf8d8f0b1c4549624ba0318e000dbe3f
Andrei Hajdukewycz <sancus@off.net>
2019-03-10 17:37:28 -0700

    Improve autoconfig repo layout and fix deploy script.

--- files/update.sh
+++ files/update.sh
@@ -5,23 +5,18 @@ DEST=/var/www/html/autoconfig.momo
 PURGEDIR=/var/www/html/autoconfig-purge
 # we generate the files in the temp dir first and then move into place to avoid temporary 404s
 TEMP=/var/www/html/autoconfig-temp
-DATA=/var/www/autoconfig.momo
-
-cd $DATA
-rm -rf convert.py
-wget https://raw.githubusercontent.com/mozilla/ispdb/master/tools/convert.py
-cd $DATA/trunk
+DATA=/var/www/autoconfig.momo/ispdb
 
 # Make sure we regenerate all the files so deleted files don't stay.
-
 mkdir -p $TEMP
 mkdir -p $TEMP/v1.1
 mkdir -p $TEMP/v1.0
 
+cd $DATA
 source /var/www/tbservices/bin/activate
-python ../convert.py -a -d $TEMP/v1.1 *
-rm -rf $DATA/trunk/cloudnine-net.jp
-python ../convert.py -a -d $TEMP/v1.0 -v 1.0 *
+python ../tools/convert.py -a -d $TEMP/v1.1 *
+rm -rf $DATA/cloudnine-net.jp
+python ../tools/convert.py -a -d $TEMP/v1.0 -v 1.0 *
 
 mv $DEST $PURGEDIR
 mv $TEMP $DEST
