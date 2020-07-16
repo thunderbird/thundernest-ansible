fe1f3fd88e4ad0bbdc62e3de326fbf92fdfddc8f
Andrei Hajdukewycz <sancus@off.net>
2020-07-16 13:02:11 -0700

    Update cache purge because Cloudflare reduced limit to 30 files at a time.

--- cacheclear.py
+++ cacheclear.py
@@ -10,17 +10,29 @@ apikey = os.environ['CF_KEY']
 email = os.environ['CF_EMAIL']
 zone = os.environ['CF_ZONE_IDENTIFIER']
 
+# Cloudflare has a 30-item limit on API cache purges now.
+def chunks(lst, n):
+    for i in xrange(0, len(lst), n):
+        yield lst[i:i + n]
+
 # Build URL list
 tb = 'https://www.thunderbird.net/'
-d = {'files': []}
+urls = []
 
 for lang in settings.PROD_LANGUAGES:
-    d['files'].append(tb+lang+'/')
+    urls.append(tb+lang+'/')
 
-d['files'].append('https://www.thunderbird.net/en-US/thunderbird/releases/')
-d['files'].append('https://www.thunderbird.net/en-US/thunderbird/all/')
-d['files'].append('https://www.thunderbird.net/media/js/common-bundle.js')
+urls.append('https://www.thunderbird.net/en-US/thunderbird/releases/')
+urls.append('https://www.thunderbird.net/en-US/thunderbird/all/')
+urls.append('https://www.thunderbird.net/media/js/common-bundle.js')
 
 cf = CloudFlare.CloudFlare(email=email, token=apikey)
-a = cf.zones.purge_cache.post(zone, data=d)
-print a
+
+lists = chunks(urls, 30) # Split into 30-item requests.
+for url_list in lists:
+    # Put it in the format the API expects.
+    api_req = {'files': url_list}
+
+    a = cf.zones.purge_cache.post(zone, data=api_req)
+    print a
+print "Completed."
